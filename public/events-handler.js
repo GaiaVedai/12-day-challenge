386b5f1ace0e0e44026fe05012615267ee956e19
GaiaVedai <GaiaVedai@gmail.com>
2018-09-04 17:24:27 +0300

    events handler

---
+++ public/events-handler.js
@@ -0,0 +1,15 @@
+class EventsHandler {
+    constructor() { //add used classes in ()
+
+    }
+
+    pickNewChellenge () {
+        $('.challenge').on('click', ()=> {
+            //let = get $this.data('type').val()
+            //move to route /startchallenge: challenge explnation and a button: startNewChallenge()
+        })
+    }
+
+}
+
+export default EventsHandler
